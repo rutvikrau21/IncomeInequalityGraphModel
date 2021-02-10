3131c4a18b0fa55b1f9ae1daa2c6298c17ec3790
kevinjiang019 <75217209+kevinjiang019@users.noreply.github.com>
2021-02-10 13:50:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import MST as mst
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def main():
     while True:
@@ -8,25 +10,35 @@ def main():
         except ValueError:  # catch the *specific* exception
             print("Enter numbers only")
 
+
+
     g = mst.Graph(num1)
     g.newMST()
-
 
 
     print("Matrix")
     print(g.graph)
     print("\n")
 
-
-    print("Distances")
+    Data = {'Edges': list(range(int((num1 * (num1 - 1))/2 - (num1 - 1)))),
+            'Efficiency': []
+            }
 
     while True:
         try:
             g.newConnection()
-            print(mst.getEfficiency(g))
+            Data['Efficiency'].append(mst.getEfficiency(g))
         except IndexError:
             print("Network fully connected")
             break
+
+    df = pd.DataFrame(Data, columns=['Edges', 'Efficiency'])
+
+    plt.plot(df['Edges'], df['Efficiency'])
+    plt.title('Efficiency vs Edges')
+    plt.xlabel('Edges added')
+    plt.ylabel('Efficiency')
+    plt.show()
 
 if __name__ == "__main__":
     main()
